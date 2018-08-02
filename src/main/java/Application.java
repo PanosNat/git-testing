aeace349aee1e6018b0ed869f02692184c7c9fe2
PanosNat <PanosNat@users.noreply.github.com>
2018-08-02 08:56:26 +0200

    Added new println commmand to Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,5 +2,6 @@ public class Application {
 public static void main(String[] args){
 	Person person = new Person("Panagiotis",34);
 	System.out.println(person);
+	System.out.println("Hello my name is Panos");
 }
 }
