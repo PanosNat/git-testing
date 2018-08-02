3da7730c2c064f9d5d125f4910a804b4cba485e6
PanosNat <PanosNat@users.noreply.github.com>
2018-08-02 09:05:56 +0200

    Added getters to class Person

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,6 +7,9 @@ public class Person {
 		this.age = age;
 	}
 
+	public String getName(){ return this.name;}
+	public int getAge() { return this.age;}
+
 	@Override
 	public String toString(){
 		return String.format("Hello my name is %s and I am %s years old",name,age);
