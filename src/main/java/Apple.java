8e1d2b49b00eb98521eab7298768f3408f66309b
Panos <peter@gmail.com>
2019-02-26 17:32:05 +0000

    Added Apple class and compiled files

---
+++ src/main/java/Apple.java
@@ -0,0 +1,18 @@
+public class Apple {
+private final int weight;
+private final String color;
+
+public Apple(int weight,String color){
+this.weight = weight;
+this.color = color;
+}
+
+public int getWeight(){
+return this.weight;
+}
+
+public String getColor(){
+return this.color;
+}
+
+}
